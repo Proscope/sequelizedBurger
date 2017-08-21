46e7787d870a39c13a1e363ba847774d5ec9f558
Harrison McCord <harrison.mccord@gmail.com>
2017-08-21 10:15:58 -0400

    added sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,28 @@
-var orm = require("../config/orm.js");
-
-var burger = {
-  selectAll: function(cb){
-    orm.selectAll(function(res){
-      cb(res);
-    });
-  },
-  insertOne: function(burger_name, cb){
-    orm.insertOne(burger_name, function(res){
-      cb(res);
-    });
-  },
-  updateOne: function(burger_id, cb){
-    orm.updateOne(burger_id, function(res){
-      cb(res);
-    });
-  }
+module.exports = function(sequelize, Datatypes) {
+  var Burgers = sequelize.define("burgers", {
+    id: {
+      type: Datatypes.INTEGER, 
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    }, 
+    burger_name: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        notEmpty: true
+      }
+    },
+    devoured: {
+      type: Datatypes.BOOLEAN,
+      defaultValue: 0
+    },
+    date: {
+      type: Datatypes.DATE,
+      defaultValue: Datatypes.NOW
+    },
+    timestamps: false
+  });
+  return Burgers;
 };
-
-module.exports = burger;
